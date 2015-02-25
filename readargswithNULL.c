9ba19810e0f24ec71516d26d6c81185fcfef38a3
BharatwajGS <bharatgso7@gmail.com>
2015-02-24 21:18:31 -0500

    Update readargswithNULL.c

--- readargswithNULL.c
+++ readargswithNULL.c
@@ -1,3 +1,12 @@
+struct Command
+{
+    struct SubCommand sub_commands[MAX_SUB_COMMANDS];
+    int num_sub_commands;
+    char *stdin_redirect;
+    char *stdout_redirect;
+    int background;
+};
+
 void readArgs(char *in, char **argv, int size)
 {
     // Initialize number of arguments
@@ -29,4 +38,53 @@ void readArgs(char *in, char **argv, int size)
           break;
         }
     }
+}
+
+void readRedirectsAndBackground(struct Command *command)
+{
+    // Assume no redirects and no background
+    command->stdin_redirect = NULL;
+    command->stdout_redirect = NULL;
+    command->background = 0;
+
+    // Nothing if no sub-commands available
+    if (!command->num_sub_commands)
+    return;
+
+    // Get last sub-command
+    struct SubCommand *sub_command = &command->sub_commands[command->num_sub_commands - 1];
+
+    // Keep extracting tokens from the last subcommand
+    while (1)
+    {
+      // Count number of arguments left
+      int argc = 0;
+      while (sub_command->argv[argc])
+      argc++;
+
+      // Extract redirect/background tokens
+      if (argc > 2 && !strcmp(sub_command->argv[argc - 2], ">"))
+      {
+        command->stdout_redirect = sub_command->argv[argc - 1];
+        sub_command->argv[argc - 2] = NULL;
+      }
+
+      else if (argc > 2 && !strcmp(sub_command->argv[argc - 2], "<"))
+      {
+        command->stdin_redirect = sub_command->argv[argc - 1];
+        sub_command->argv[argc - 2] = NULL;
+      }
+
+      else if (argc > 1 && !strcmp(sub_command->argv[argc - 1], "&"))
+      {
+        command->background = 1;
+        sub_command->argv[argc - 1] = NULL;
+      }
+    
+      else
+      {
+        // No more redirects/background
+        break;
+      }
+  }
 }
